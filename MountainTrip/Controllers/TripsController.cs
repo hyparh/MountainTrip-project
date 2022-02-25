9dfd9fbb07a77c5fe39c86e1d26b3727fe73ea77
hyparh <hyparh@gmail.com>
2022-02-25 23:20:37 +0200

    Add TripController and Trip view

---
+++ MountainTrip/Controllers/TripsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MountainTrip.Models.Trips;
+
+namespace MountainTrip.Controllers
+{
+    public class TripsController : Controller
+    {
+        public IActionResult Add() => View();
+
+        // model binding
+        [HttpPost]
+        public IActionResult Add(AddTripFormModel trip)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ MountainTrip/Models/Trips/AddTripFormModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MountainTrip.Models.Trips
+{
+    public class AddTripFormModel
+    {       
+        public string Name { get; init; }
+        
+        public string Description { get; init; }
+
+        public double Length { get; init; }
+
+        public string Duration { get; init; }
+
+        public string Difficulty { get; init; }
+    
+        public string ImageUrl { get; init; }
+
+        public int MountainId { get; init; }
+    }
+}

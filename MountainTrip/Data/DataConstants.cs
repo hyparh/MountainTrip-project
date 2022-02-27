9cf2ef993d526b102c871891b99342923ea87c75
hyparh <hyparh@gmail.com>
2022-02-27 13:21:32 +0200

    Correcting the Save button in Add view to be type="submit"

--- MountainTrip/Data/DataConstants.cs
+++ MountainTrip/Data/DataConstants.cs
@@ -3,7 +3,9 @@
     public class DataConstants
     {
         // Trip
+        public const int TripNameMinLength = 5;        
         public const int TripNameMaxLength = 50;        
+        public const int TripDescriptionMinLength = 10;        
 
         // Mountain
         public const int MountainNameMaxLength = 20;
--- MountainTrip/Models/Trips/AddTripFormModel.cs
+++ MountainTrip/Models/Trips/AddTripFormModel.cs
@@ -1,11 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MountainTrip.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace MountainTrip.Models.Trips
 {
+    using static DataConstants;
+
     public class AddTripFormModel
-    {       
+    {
+        [Required]
+        [MinLength(TripNameMinLength)]
+        [MaxLength(TripNameMaxLength)]
         public string Name { get; init; }
-        
+
+        [Required]
+        [MinLength(TripDescriptionMinLength)]
         public string Description { get; init; }
 
         public double Length { get; init; }
@@ -13,10 +21,13 @@ namespace MountainTrip.Models.Trips
         public string Duration { get; init; }
 
         public string Difficulty { get; init; }
-    
+
+        [Required]
         [Display(Name = "Image URL")]
+        [Url]
         public string ImageUrl { get; init; }
 
+        [Required]
         [Display(Name = "Mountain")]
         public int MountainId { get; init; }
 

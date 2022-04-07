6362334dd703f3a6265a5ce3d9c870787c9874b9
hyparh <hyparh@gmail.com>
2022-04-07 13:10:14 +0300

    Added memory cache for HomeController

--- MountainTrip/Controllers/HomeController.cs
+++ MountainTrip/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using MountainTrip.Data;
 using MountainTrip.Services.Home;
 using MountainTrip.Services.Trips;
@@ -10,31 +11,46 @@ namespace MountainTrip.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly MountainTripDbContext data;
+        private readonly MountainTripDbContext data;       
         private readonly IStatisticsService statistics;
+        private readonly IMemoryCache cache;
         private readonly IMapper mapper;
 
         public HomeController(
             MountainTripDbContext data,
-            IStatisticsService statistics,            
+            IStatisticsService statistics,
+            IMemoryCache cache,
             IMapper mapper)
         {
             this.data = data;
             this.statistics = statistics;
             this.mapper = mapper;
+            this.cache = cache;
         }
 
         public IActionResult Index()
         {
-            var totalTrips = data.Trips.Count();
-            var totalUsers = data.Users.Count();
-            var totalBookings = data.Bookings.Count();
+            const string latestTripsCacheKey = "LatestTripsCacheKey";
 
-            var trips = data.Trips
+            var latestTrips = cache.Get<List<LatestTripServiceModel>>(latestTripsCacheKey);
+
+            if (latestTrips is null)
+            {
+                latestTrips = data.Trips
                 .OrderByDescending(t => t.Id)
                 .ProjectTo<LatestTripServiceModel>(mapper.ConfigurationProvider)
                 .Take(3)
                 .ToList();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(15));
+
+                cache.Set(latestTripsCacheKey, latestTrips, cacheOptions);
+            }
+
+            var totalTrips = data.Trips.Count();
+            var totalUsers = data.Users.Count();
+            var totalBookings = data.Bookings.Count();           
 
             var totalStatistics = statistics.Total();            
 
@@ -43,7 +59,7 @@ namespace MountainTrip.Controllers
                 TotalTrips = totalStatistics.TotalTrips,
                 TotalUsers = totalStatistics.TotalUsers,
                 TotalBookings = totalStatistics.TotalBookings,
-                Trips = trips
+                Trips = latestTrips
             });
         }        
 
